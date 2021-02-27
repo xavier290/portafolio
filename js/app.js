62f7b70879262ce1c1c3c7e284ce9233539f0f17
xavier <xa25alex@gmail.com>
2021-02-26 19:30:17 -0600

    almost finished with everything

--- js/app.js
+++ js/app.js
@@ -1,35 +1,52 @@
 const container = document.querySelector('.container');
 const header = document.querySelector('.header');
+const menuBtn = document.querySelector('.menu-btn');
+const openMenu = document.querySelector('.menu');
+
+let menuOpen = false;
 
 const tl = new TimelineMax();
 
-tl.fromTo(container, 1, {opacity: '0',}, {opacity: '1', ease: Power2.easeInOut}, '0.2');
+tl.fromTo(container, 1, {opacity: '0',}, {opacity: '1', ease: Power2.easeInOut}, '0.1');
 
 $(document).ready( () => {
 
     $(window).scroll( () => {
         var scrollPosition = $(this).scrollTop();
 
-        if (scrollPosition > 550){
+        if (scrollPosition > 500){
             $('header').css('height', '0');
-        } else {
+        } 
+        else {
             $('header').css('height', '14vh');
+        }
+
+        if (scrollPosition > 600) {
+            $('.go-up').css('display', 'block');
+        }
+        else {
+            $('.go-up').css('display', 'none');
+        }
+
+        if(screen.width < 1000) {
+            if(scrollPosition > 400) {
+                $('.menu').css('display', 'none');
+            } 
+            else {
+                $('.menu').css('display', '')
+            }
         }
     })
 })
 
 // toggle menu in mobile devices
 
-const menuBtn = document.querySelector('.menu-btn');
-let menuOpen = false;
-
-const openMenu = document.querySelector('.menu');
-
 menuBtn.addEventListener('click', () => {
     if(!menuOpen) {
         menuBtn.classList.add('open');
         menuOpen = true;
-    } else {
+    } 
+    else {
         menuBtn.classList.remove('open');
         menuOpen = false;
     }
